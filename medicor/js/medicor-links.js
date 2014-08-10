cb82a0ad36a85cd48bfffb5d3becbe3284935dcf
aboritskiy <anton.boritskiy@sitewards.com>
2014-08-10 14:25:20 +0200

    - converted to unicode - added page load events tracking - added open graph tags

--- medicor/js/medicor-links.js
+++ medicor/js/medicor-links.js
@@ -1,9 +1,11 @@
 /*
  @module medicor-links.js
- This module tracks link clicks
+ This module tracks link clicks and a bit more
  */
 (function ($) {
     $(document).ready(function () {
+        ga('send', 'event', 'document', 'ready', 'index');
+
         $('a.tel').on('mousedown', function () {
             ga('send', 'event', 'link', 'click', 'telephone');
         });
@@ -15,5 +17,9 @@
         $('a.price').on('click', function () {
             ga('send', 'event', 'link', 'click', 'price');
         });
+    });
+
+    $(window).on('load', function () {
+        ga('send', 'event', 'window', 'load', 'index');
     });
 })(jQuery);
